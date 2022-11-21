5a9b7fd33e799091fd324c97d8dba265694502fb
Julian Canepa <julian.l.canepa@gmail.com>
2022-11-21 10:59:54 -0800

    Move unsupported demonstrationt to end of main file.

--- main09.rb
+++ main09.rb
@@ -3,10 +3,14 @@
 require_relative 'employee'
 require_relative 'bike'
 
-bikes = [BmxBike.new, RoadBike.new, MountainBike.new, Tricycle.new, Foocycle.new]
+bikes = [BmxBike.new, RoadBike.new, MountainBike.new, Tricycle.new]
 
 employee = Employee.new
 
 bikes.each do |bike|
   employee.prepare(bike)
 end
+
+# demonstrate a class that doesn't adhere to the bike abstraction (interface)
+unsupported_bike = Foocycle.new
+employee.prepare(unsupported_bike)
